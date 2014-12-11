2076edd489310df2abdb95c71e17ed62d9326166
Markus Grotz <markus.grotz@kit.edu>
2014-12-11 14:16:04 +0100

    initial commit for YarpSegmentationProvider

---
+++ source/WalkmanBridge/components/YarpSegmentationProvider/YarpSegmentationProvider.h
@@ -0,0 +1,95 @@
+/*
+ * This file is part of ArmarX.
+ *
+ * ArmarX is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * ArmarX is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package    WalkmanBridge::ArmarXObjects::YarpSegmentationProvider
+ * @author     Markus Grotz ( markus dot grotz at kit dot edu )
+ * @date       2014
+ * @copyright  http://www.gnu.org/licenses/gpl.txt
+ *             GNU General Public License
+ */
+
+#ifndef _ARMARX_COMPONENT_WalkmanBridge_YarpSegmentationProvider_H
+#define _ARMARX_COMPONENT_WalkmanBridge_YarpSegmentationProvider_H
+
+
+#include <Core/core/Component.h>
+
+namespace armarx
+{
+    /**
+     * @class YarpSegmentationProviderPropertyDefinitions
+     * @brief
+     * @ingroup Components
+     */
+    class YarpSegmentationProviderPropertyDefinitions:
+        public ComponentPropertyDefinitions
+    {
+    public:
+        YarpSegmentationProviderPropertyDefinitions(std::string prefix):
+            ComponentPropertyDefinitions(prefix)
+        {
+            //defineRequiredProperty<std::string>("PropertyName", "Description");
+            //defineOptionalProperty<std::string>("PropertyName", "DefaultValue", "Description");
+        }
+    };
+
+    /**
+     * @class YarpSegmentationProvider
+     * @brief A brief description
+     *
+     * Detailed Description
+     */
+    class YarpSegmentationProvider :
+        virtual public armarx::Component
+    {
+    public:
+        /**
+         * @see armarx::ManagedIceObject::getDefaultName()
+         */
+        virtual std::string getDefaultName() const
+        {
+            return "YarpSegmentationProvider";
+        }
+
+    protected:
+        /**
+         * @see armarx::ManagedIceObject::onInitComponent()
+         */
+        virtual void onInitComponent();
+
+        /**
+         * @see armarx::ManagedIceObject::onConnectComponent()
+         */
+        virtual void onConnectComponent();
+
+        /**
+         * @see armarx::ManagedIceObject::onDisconnectComponent()
+         */
+        virtual void onDisconnectComponent();
+
+        /**
+         * @see armarx::ManagedIceObject::onExitComponent()
+         */
+        virtual void onExitComponent();
+
+        /**
+         * @see PropertyUser::createPropertyDefinitions()
+         */
+        virtual PropertyDefinitionsPtr createPropertyDefinitions();
+    };
+}
+
+#endif
